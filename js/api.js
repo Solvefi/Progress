5cf35dbd262b146562a69b797fe69dbe23a08379
Solvefi <masha567777777@gmail.com>
2025-11-25 17:24:26 +0300

    add animate state

--- js/api.js
+++ js/api.js
@@ -2,14 +2,28 @@ const circle = document.querySelector('.progress-bar');
 const radius = circle.r.baseVal.value;
 const circumference = 2* Math.PI * radius;
 const valueInput = document.getElementById('value');
+const animateChekbox = document.getElementById('animate');
+const circleProgress = document.querySelector('.progress-circle')
+
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`
+circle.style.strokeDashoffset = circumference;
+
+
 
 valueInput.addEventListener('change', function() {
     const value = parseInt(valueInput.value) || 0;
     setProgress(value);
 });
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`
-circle.style.strokeDashoffset = circumference;
+animateChekbox.addEventListener('change', function() {
+    if (this.checked){
+        circle.classList.add('animating')
+    } else {
+        circle.classList.remove('animating')
+    }
+
+});
 
 function setProgress (percent){
     const offset = circumference - percent/100 * circumference;

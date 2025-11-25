eb21b49d26e344fc3e4c705a52425d53263ee3bb
Solvefi <masha567777777@gmail.com>
2025-11-25 17:48:21 +0300

    add hide state

--- js/api.js
+++ js/api.js
@@ -2,8 +2,9 @@ const circle = document.querySelector('.progress-bar');
 const radius = circle.r.baseVal.value;
 const circumference = 2* Math.PI * radius;
 const valueInput = document.getElementById('value');
-const animateChekbox = document.getElementById('animate');
-const circleProgress = document.querySelector('.progress-circle')
+const animateCheckbox = document.getElementById('animate');
+const circleProgress = document.querySelector('.circular-progress')
+const hideCheckbox = document.getElementById('hide');
 
 
 circle.style.strokeDasharray = `${circumference} ${circumference}`
@@ -16,11 +17,20 @@ valueInput.addEventListener('change', function() {
     setProgress(value);
 });
 
-animateChekbox.addEventListener('change', function() {
+animateCheckbox.addEventListener('change', function() {
     if (this.checked){
         circle.classList.add('animating')
     } else {
         circle.classList.remove('animating')
+    }
+
+});
+
+hideCheckbox.addEventListener('change', function() {
+    if (this.checked){
+        circleProgress.classList.add('hidden')
+    } else {
+        circleProgress.classList.remove('hidden')
     }
 
 });
